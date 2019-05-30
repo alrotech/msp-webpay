507681b50f9263abeba3aea0baad2ab65c430faf
Ivan Klimchuk <ivan@klimchuk.com>
2019-05-30 04:11:50 +0300

    Get modx and xpdo for building process by composer

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -1,67 +1,116 @@
 <?php
-/**
- * mspWebPay build script
- *
- * @package mspwebpay
- * @subpackage build
- */
-$mtime = microtime();
-$mtime = explode(' ', $mtime);
-$mtime = $mtime[1] + $mtime[0];
-$tstart = $mtime;
+
 set_time_limit(0);
+error_reporting(E_ALL | E_STRICT); ini_set('display_errors',true);
 
-require_once 'build.config.php';
+ini_set('date.timezone', 'Europe/Minsk');
 
-/* define sources */
-$root = dirname(dirname(__FILE__)).'/';
-$sources = array(
-    'root' => $root,
+define('PKG_NAME', 'mspWebPay');
+define('PKG_NAME_LOWER', strtolower(PKG_NAME));
+define('PKG_VERSION', '2.0.0');
+define('PKG_RELEASE', 'pl');
+
+require_once __DIR__ . '/vendor/modx/revolution/core/xpdo/xpdo.class.php';
+
+/* instantiate xpdo instance */
+$xpdo = new xPDO('mysql:host=localhost;dbname=modx;charset=utf8', 'root', '',
+    [xPDO::OPT_TABLE_PREFIX => 'modx_', xPDO::OPT_CACHE_PATH => __DIR__ . '/../../../core/cache/'],
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]
+);
+$cacheManager= $xpdo->getCacheManager();
+$xpdo->setLogLevel(xPDO::LOG_LEVEL_INFO);
+$xpdo->setLogTarget();
+
+$root = dirname(__DIR__) . '/';
+$sources = [
     'build' => $root . '_build/',
     'data' => $root . '_build/data/',
+    'docs' => $root . 'docs/',
     'resolvers' => $root . '_build/resolvers/',
-    'source_assets' => array(
-        'components/minishop2/payment/webpay.php'
-    ),
-    'source_core' => array(
-        'components/minishop2/custom/payment/webpay.class.php',
+    'validators' => $root . '_build/validators/',
+    'implants' => $root . '_build/implants/',
+    'plugins' => $root . 'core/components/' . PKG_NAME_LOWER . '/elements/plugins/',
+    'assets' => [
+        'components/mspwebpay/'
+    ],
+    'core' => [
+        'components/mspwebpay/',
         'components/minishop2/lexicon/en/msp.webpay.inc.php',
         'components/minishop2/lexicon/ru/msp.webpay.inc.php'
-    )
-,'docs' => $root . 'docs/'
-);
-require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+    ],
+];
 
-$modx= new modX();
-$modx->initialize('mgr');
-echo '<pre>'; /* used for nice formatting of log messages */
-$modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->setLogTarget('ECHO');
+$signature = implode('-', [PKG_NAME_LOWER, PKG_VERSION, PKG_RELEASE]);
 
-$modx->loadClass('transport.modPackageBuilder','',false, true);
-$builder = new modPackageBuilder($modx);
-$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
-$modx->log(modX::LOG_LEVEL_INFO,'Created Transport Package.');
+$release = false;
+if (!empty($argv) && $argc > 1) {
+    $release = $argv[1];
+}
+
+$directory = $release === 'release' ? $root . '_packages/' : __DIR__ . '/../../../core/packages/';
+$filename = $directory . $signature . '.transport.zip';
+
+/* remove the package if it's already been made */
+if (file_exists($filename)) {
+    unlink($filename);
+}
+if (file_exists($directory . $signature) && is_dir($directory . $signature)) {
+    $cacheManager = $xpdo->getCacheManager();
+    if ($cacheManager) {
+        $cacheManager->deleteTree($directory . $signature, true, false, []);
+    }
+}
+
+$xpdo->loadClass('transport.xPDOTransport', XPDO_CORE_PATH, true, true);
+
+$package = new xPDOTransport($xpdo, $signature, $directory);
+$xpdo->log(xPDO::LOG_LEVEL_INFO, 'Transport package is created.');
+
+$xpdo->setPackage('modx', __DIR__ . '/vendor/modx/revolution/core/model/');
+$xpdo->loadClass('modAccess');
+$xpdo->loadClass('modAccessibleObject');
+$xpdo->loadClass('modAccessibleSimpleObject');
+$xpdo->loadClass('modPrincipal');
+
+$namespace = $xpdo->newObject('modNamespace');
+$namespace->fromArray([
+    'id' => PKG_NAME_LOWER,
+    'name' => PKG_NAME_LOWER,
+    'path' => '{core_path}components/' . PKG_NAME_LOWER . '/',
+]);
+
+$package->put($namespace, [
+//    'vehicle_class' => EncryptedVehicle::class,
+    xPDOTransport::UNIQUE_KEY => 'name',
+    xPDOTransport::PRESERVE_KEYS => true,
+    xPDOTransport::UPDATE_OBJECT => true,
+    xPDOTransport::NATIVE_KEY => PKG_NAME_LOWER,
+    'namespace' => PKG_NAME_LOWER
+]);
+
+$package->pack();
+
+exit();
 
 /* load system settings */
-if (defined('BUILD_SETTING_UPDATE')) {
-    $settings = include $sources['data'].'transport.settings.php';
-    if (!is_array($settings)) {
-        $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in settings.');
-    } else {
-        $attributes= array(
-            xPDOTransport::UNIQUE_KEY => 'key',
-            xPDOTransport::PRESERVE_KEYS => true,
-            xPDOTransport::UPDATE_OBJECT => BUILD_SETTING_UPDATE,
-        );
-        foreach ($settings as $setting) {
-            $vehicle = $builder->createVehicle($setting,$attributes);
-            $builder->putVehicle($vehicle);
-        }
-        $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($settings).' System Settings.');
-    }
-    unset($settings,$setting,$attributes);
-}
+//if (defined('BUILD_SETTING_UPDATE')) {
+//    $settings = include $sources['data'].'transport.settings.php';
+//    if (!is_array($settings)) {
+//        $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in settings.');
+//    } else {
+//        $attributes= array(
+//            xPDOTransport::UNIQUE_KEY => 'key',
+//            xPDOTransport::PRESERVE_KEYS => true,
+//            xPDOTransport::UPDATE_OBJECT => BUILD_SETTING_UPDATE,
+//        );
+//        foreach ($settings as $setting) {
+//            $vehicle = $builder->createVehicle($setting,$attributes);
+//            $builder->putVehicle($vehicle);
+//        }
+//        $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($settings).' System Settings.');
+//    }
+//    unset($settings,$setting,$attributes);
+//}
 
 /* @var msPayment $payment */
 $payment= $modx->newObject('msPayment');
@@ -131,52 +180,5 @@ $modx->log(modX::LOG_LEVEL_INFO,'Added package attributes and setup options.');
 /* zip up package */
 $modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
 $builder->pack();
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />");
 
-$mtime= microtime();
-$mtime= explode(" ", $mtime);
-$mtime= $mtime[1] + $mtime[0];
-$tend= $mtime;
-$totalTime= ($tend - $tstart);
-$totalTime= sprintf("%2.4f s", $totalTime);
-
-$signature = $builder->getSignature();
-if (defined('PKG_AUTO_INSTALL') && PKG_AUTO_INSTALL) {
-    $sig = explode('-',$signature);
-    $versionSignature = explode('.',$sig[1]);
-
-    /* @var modTransportPackage $package */
-    if (!$package = $modx->getObject('transport.modTransportPackage', array('signature' => $signature))) {
-        $package = $modx->newObject('transport.modTransportPackage');
-        $package->set('signature', $signature);
-        $package->fromArray(array(
-                'created' => date('Y-m-d h:i:s'),
-                'updated' => null,
-                'state' => 1,
-                'workspace' => 1,
-                'provider' => 0,
-                'source' => $signature.'.transport.zip',
-                'package_name' => $sig[0],
-                'version_major' => $versionSignature[0],
-                'version_minor' => !empty($versionSignature[1]) ? $versionSignature[1] : 0,
-                'version_patch' => !empty($versionSignature[2]) ? $versionSignature[2] : 0,
-            ));
-        if (!empty($sig[2])) {
-            $r = preg_split('/([0-9]+)/',$sig[2],-1,PREG_SPLIT_DELIM_CAPTURE);
-            if (is_array($r) && !empty($r)) {
-                $package->set('release',$r[0]);
-                $package->set('release_index',(isset($r[1]) ? $r[1] : '0'));
-            } else {
-                $package->set('release',$sig[2]);
-            }
-        }
-        $package->save();
-    }
-    $package->install();
-}
-if (!empty($_GET['download'])) {
-    echo '<script>document.location.href = "/core/packages/' . $signature.'.transport.zip' . '";</script>';
-}
-
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Execution time: {$totalTime}\n");
-echo '</pre>';
+$modx->log(modX::LOG_LEVEL_INFO, 'Package Built.');
